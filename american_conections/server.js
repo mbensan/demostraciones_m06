44c2d9cab516d301643582a735ba0212dfd045c8
Matias Bensan A <mbensan@gmail.com>
2022-08-01 10:13:15 -0400

    Resolución ejercicio American Conections

---
+++ american_conections/server.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const fs = require('fs').promises
+
+const app = express()
+
+// para usar archivos estáticos
+app.use(express.static('public'))
+
+// para leer datos de formulario
+app.use(express.json());       // to support JSON-encoded bodies
+app.use(express.urlencoded());
+
+function getForm(req) {
+  return new Promise((res, rej) => {
+    let str = ''
+    req.on('data', function (chunk) {
+      str += chunk
+    })
+    req.on('end', function () {
+      console.log('str', str);
+      const obj = JSON.parse(str)
+      res(obj)
+    })
+  })
+}
+
+// RUTAS
+/*
+app.post('/pizza', async (req, res) => {
+  const datos = await getForm(req)
+  console.log(datos);
+  res.json({tudo: 'bem'})
+});
+*/
+
+app.get('/crear', async (req, res) => {
+  // 1. recuperar los datos del formulario
+  const nombre = req.query.nombre
+  const contenido = req.query.contenido
+
+  const hoy = new Date()
+  // 2. creamos el nuevo archivo
+  await fs.writeFile(`files/${nombre}.txt`, `${hoy}\n${contenido}`, 'utf-8')
+  // 3. retornamos una respuesta al usuario
+  res.send('Archivo creado')
+})
+
+app.get('/leer', async (req, res) => {
+  // 1. Recuperamos datos del formulario
+  const nombre = req.query.nombre
+
+  // 2. Leemos el archivo
+  let contenido = await fs.readFile(`files/${nombre}.txt`, 'utf-8')
+
+  // 2.5 Agregamos saltos de línea
+  contenido = contenido.split('\n').join('<br>')
+  
+  // 3. retornamos una respuesta al usuario
+  res.send(contenido)
+})
+
+
+app.get('*', (req, res) => {
+  res.send('Página aún no implementada')
+});
+
+app.listen(3000, function () {
+  console.log('servidor ejecutando correctamente');
+})
