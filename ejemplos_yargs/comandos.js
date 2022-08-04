6651d101bd81eb01d64731bc6fb325cb8e68a148
Matias Bensan A <mbensan@gmail.com>
2022-08-04 09:32:43 -0400

    Envio de mails usando nodemailer.

--- ejemplos_yargs/comandos.js
+++ ejemplos_yargs/comandos.js
@@ -1,4 +1,5 @@
 const yargs = require('yargs')
+const enviar_email = require('./email.js').enviar
 
 
 const argv = yargs.command(
@@ -17,5 +18,55 @@ const argv = yargs.command(
     } else {
       console.log('Hola, que tal!');
     }
+  }
+).help().argv
+
+
+const argv2 = yargs.command(
+  'frase',
+  'Comando para buscar una frase de GOT',
+  {
+    casa: {
+      describe: 'Para poder definir el ID de la casa',
+      demand: false,
+      alias: 'c'
+    }
+  },
+  function (args) {
+    let casa = args.casa
+    if (! args.casa) {
+      casa = Math.ceil(Math.random() * 80)
+    }
+    console.log(casa);
+    // ahora vamos a consultar a la API
+  }
+).help().argv
+// 107.2145
+
+const argv3 = yargs.command(
+  'email',
+  'Comando para probar el envio de emails',
+  {
+    asunto: {
+      describe: 'asunto del email a ennviar',
+      demand: true,
+      alias: 'a'
+    },
+    mensaje: {
+      describe: 'contenido del email a enviar',
+      demand: true,
+      alias: 'm'
+    },
+    receptor: {
+      describe: 'receptor del email',
+      demand: false,
+      alias: 'r'
+    }
+  },
+  function (args) {
+    const receptor = args.receptor || 'mbensan@outlook.com'
+
+    console.log('Enviando correo electrónico');
+    enviar_email(receptor, args.asunto, args.mensaje)
   }
 ).help().argv
---
+++ ejemplos_yargs/email.js
@@ -0,0 +1,35 @@
+const nodemailer = require('nodemailer')
+
+const transport = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'mbensan.test@gmail.com',
+    pass: 'dgdxbpvqozekxwkn'
+  }
+})
+
+function enviar (receivers, asunto, mensaje) {
+  const options = {
+    from: 'mbensan.test@gmail.com',
+    to: receivers,
+    subject: asunto,
+    html: mensaje
+  }
+
+  transport.sendMail(options, function (err, info) {
+    if (err) {
+      console.log('enviading');
+      console.log('error', err);
+    } else {
+      console.log(info);
+    }
+  })
+}
+
+const por_defecto = ['mbensan@outlook.com', 'mbensan@ug.uchile.cl']
+
+module.exports = {
+  por_defecto,
+  enviar
+}
+
